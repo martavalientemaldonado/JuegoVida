4ffad2fdb3b91fa949d525584aab0cec031e6a22
UAH <martavalientemaldonado@gmail.com>
2024-05-04 11:22:05 +0200

    hhh

--- src/main/java/com/example/juegovida/Utilities/Paths.java
+++ src/main/java/com/example/juegovida/Utilities/Paths.java
@@ -5,5 +5,5 @@ import com.example.juegovida.App.Bienvenida;
 public class Paths {
     public static final String BIENVENIDA= "C:\\Users\\UAH\\JuegoVida\\src\\main\\resources\\bienvenida.fxml";
     public static final String PANTALLAINICIO="C:\\Users\\UAH\\JuegoVida\\src\\main\\resources\\PantallaInicio.fxml";
-     public static final String PARAMINDIV="C:\\Users\\UAH\\JuegoVida\\src\\main\\resources\\parametrosIndiv.fxml";
+    public static final String PARAMINDIV="C:\\Users\\UAH\\JuegoVida\\src\\main\\resources\\parametrosIndiv.fxml";
 }
